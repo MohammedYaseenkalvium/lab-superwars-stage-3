088babd1c20bd31ea04dab269b161b3ebac4385e
Mohammed Yaseen <mohammed.yaseen@kalvium.community>
2024-05-01 15:17:18 +0530

    super wars stage 3 is also done

--- src/app.js
+++ src/app.js
@@ -2,58 +2,84 @@ const PLAYERS = [
     "Spiderman",
     "Captain America",
     "Wonderwoman",
-    "Popcorn",
+    "Dookey",
     "Gemwoman",
-    "Bolt",
-    "Antwoman",
-    "Mask",
+    "Bolter",
+    "Ironwoman",
+    "Flash",
     "Tiger",
-    "Captain",
+    "Captain Underpants",
     "Catwoman",
-    "Fish",
+    "Aquaman",
     "Hulk",
-    "Ninja",
-    "Black Cat",
-    "Volverine",
+    "Deadpool",
+    "Black Panther",
+    "Doctor Strange",
     "Thor",
     "Slayer",
     "Vader",
-    "Slingo"
-];
-
-// initialize players with image and strength
-const initPlayers = (players) => {
-    let detailedPlayers = '';
-
+    "Thanos",
+  ];
+  
+  // initialize players with image and strength
+  const initPlayers = (players) => {
+    let detailedPlayers = [];
+  
     // Instead of forloop use Map method
     // Code here
-
+      players.map((curr, i) => {
+        let hero_or_villain = i%2==0 ? "hero":"villain"
+        let obj = {
+        name: curr,
+        strength: getRandomStrength(),
+        image: `./images/super-${i+1}.png`,
+        type: hero_or_villain,
+      };
+      detailedPlayers.push(obj)
+      return detailedPlayers
+    });
+    console.log(detailedPlayers);
     return detailedPlayers;
-}
-
-// getting random strength
-const getRandomStrength = () => {
+  };
+  
+  
+  // getting random strength
+  const getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
-}
-
-// Build player template
-const buildPlayers = (players, type) => {
-    let fragment = '';
-
+  };
+  
+  // Build player template
+  const buildPlayers = (players, type) => {
+    let fragment = "";
+  
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
+    let super_arr = players.filter((curr)=>{
+      return curr.type == type
+    })
+  
+    let div_arr = super_arr.map((curr)=>{
+      let div = `<div class="player">
+                     <img src="${curr.image}" alt="">
+                     <div class="name">${curr.name}</div>
+                     <div class="strength">${curr.strength}</div>
+                 </div>`
+  
+      return div
+    })
+    
+    fragment = div_arr.join("")
+   
     return fragment;
-}
-
-// Display players in HTML
-const viewPlayers = (players) => {
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-}
-
-
-window.onload = () => {
+  };
+  
+  // Display players in HTML
+  const viewPlayers = (players) => {
+    document.getElementById("heroes").innerHTML = buildPlayers(players, "hero");
+    document.getElementById("villains").innerHTML = buildPlayers(players,"villain");
+  };
+  
+  window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
-}
+  };
